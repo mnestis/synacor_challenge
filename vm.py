b0bb01203a87edfd80a0a2706a6b33b9b2d59b8b
Darren Richardson <mnestis@users.noreply.github.com>
2015-12-16 04:50:39 +0000

    Added jf opcode, as well as a debug functionality.

--- vm.py
+++ vm.py
@@ -5,12 +5,13 @@ import sys
 
 class VirtualMachine():
     
-    def __init__(self):
+    def __init__(self, debug=False):
         print "Booting MnesisVM..."
         self.memory = numpy.zeros((32775,), dtype=numpy.dtype("<u2")) # Includes the registers 
         self.stack = []
         self.running = False
         self.program_counter = 0
+        self.debug = debug
         print "Ready to go!"
 
     def load_program(self, filename):
@@ -20,15 +21,20 @@ class VirtualMachine():
         print "Program loaded."
 
     def run(self):
-        print "Running program:"
+        print "\nRunning program:"
         self.running = True
         print "--------------------S T A R T--------------------"
         while self.running:
+            if self.debug==True:
+                next_op = self.memory[self.program_counter]
+                print "DEBUG *** PC: %s, OP: %s, ARGS:" % (self.program_counter, VirtualMachine.debug_values[next_op]["name"]),
+                print self.memory[self.program_counter+1: self.program_counter + VirtualMachine.debug_values[next_op]["args"] + 1] 
             VirtualMachine.opcodes[self.memory[self.program_counter]](self)
         print "-------------------- H A L T --------------------"
 
     def halt(self): # OPCODE 0
         """ Stops the program from running. """
+        self.program_counter += 1
         self.running = False
 
     def jmp(self): # OPCODE 6
@@ -38,6 +44,13 @@ class VirtualMachine():
     def jt(self): # OPCODE 7
         """ If a is not 0, jump to b. """
         if self.memory[self.program_counter+1] != 0:
+            self.program_counter = self.memory[self.program_counter+2]
+        else:
+            self.program_counter += 3
+
+    def jf(self): # OPCODE 8
+        """ If a is 0, jump to b. """
+        if self.memory[self.program_counter+1] == 0:
             self.program_counter = self.memory[self.program_counter+2]
         else:
             self.program_counter += 3
@@ -55,12 +68,28 @@ class VirtualMachine():
     opcodes = {0: halt,
                6: jmp,
                7: jt,
+               8: jf,
                19: out,
-               21: noop}
+               21: noop,
+               }
 
+    debug_values = {0: {"name": "halt",
+                        "args": 0},
+                    6: {"name": "jmp",
+                        "args": 1},
+                    7: {"name": "jt",
+                        "args": 2},
+                    8: {"name": "jf",
+                        "args": 2},
+                    19: {"name": "out",
+                         "args": 1},
+                    21: {"name": "noop",
+                         "args": 0},
+                    }
 
 if __name__=="__main__":
 
-    vm = VirtualMachine()
+    vm = VirtualMachine(debug=False)
     vm.load_program("challenge.bin")
+    vm.run()
     vm.run()
