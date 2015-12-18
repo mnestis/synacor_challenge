831a52e5f4fce94d51c9f39816ac52b05e7d2b9e
Darren Richardson <mnestis@users.noreply.github.com>
2015-12-18 12:49:33 +0000

    Moved the secret.py script and added some notes.

---
+++ hack_teleporter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+"""
+This is the subroutine at index 6027. From the book in the office, I understand
+that I need to find out which value of c (r7 or 32775) that produces a certain answer.
+
+From 5491 I see that the value I'm looking for is 6.
+"""
+
+def magic(a, b, c):
+
+    if a == 0:
+        return (b + 1) % 32768
+
+    else:
+        if b == 0:
+            return magic(a - 1, c, c)
+        else:
+            return magic(a - 1, magic(a, b -1, c), c)
+            
+if __name__=="__main__":
+    for c in range(256):
+        output = magic(4, 1, c)
+        print output
